3344b9dd82a2af1320823c13c820220247ea2c12
Mark Lindamood <mark.lindamood@yahoo.com>
2017-09-13 19:02:11 -0700

    Set up MainVC including attributed text field

--- window-shopper/Controller/MainVC.swift
+++ window-shopper/Controller/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  window-shopper
 //
 //  Created by Mark Lindamood on 9/13/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ window-shopper/Views/CurrencyTextField.swift
@@ -0,0 +1,25 @@
+//
+//  CurrencyTextField.swift
+//  window-shopper
+//
+//  Created by Mark Lindamood on 9/13/17.
+//  Copyright © 2017 udemy. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTextField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: UIColor.white])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+}
